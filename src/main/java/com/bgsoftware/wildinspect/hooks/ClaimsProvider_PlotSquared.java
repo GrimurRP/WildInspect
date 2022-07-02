78e214e5a1f245544774ba49d553ff40eb8a4669
Drc-DEV <dracarys.pro@gmail.com>
2022-07-02 12:15:45 +0200

    Update PlotSquared Hook

--- src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_PlotSquared.java
+++ src/main/java/com/bgsoftware/wildinspect/hooks/ClaimsProvider_PlotSquared.java
@@ -1,9 +1,11 @@
 package com.bgsoftware.wildinspect.hooks;
 
-import com.plotsquared.core.PlotSquared;
+import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.plot.Plot;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+
+import java.util.Arrays;
 
 public final class ClaimsProvider_PlotSquared implements ClaimsProvider {
 
@@ -13,18 +15,15 @@ public final class ClaimsProvider_PlotSquared implements ClaimsProvider {
     }
 
     @Override
-    public boolean hasRole(Player player, Location location, String... roles) {
-        return true;
+    public boolean hasRole(Player player, Location bukkitLocation, String... role) {
+        Plot plot = BukkitUtil.adaptComplete(bukkitLocation).getOwnedPlot();
+        return plot != null && Arrays.asList(role).contains(plot.isOwner(player.getUniqueId()) ? "OWNER" : "TRUSTED");
     }
 
     @Override
-    public boolean hasRegionAccess(Player player, Location location) {
-        com.plotsquared.core.location.Location loc = com.plotsquared.core.location.Location.at(
-                location.getWorld().getName(),
-                location.getBlockX(), location.getBlockY(), location.getBlockZ(), 0F, 0F);
-        Plot plot = PlotSquared.get().getPlotAreaManager().getPlotArea(loc).getOwnedPlot(loc);
-        if (plot == null) return false;
-        return plot.isAdded(player.getUniqueId()) || plot.isOwner(player.getUniqueId());
+    public boolean hasRegionAccess(Player player, Location bukkitLocation) {
+        Plot plot = BukkitUtil.adaptComplete(bukkitLocation).getOwnedPlot();
+        return plot != null && (plot.isAdded(player.getUniqueId()) || plot.isOwner(player.getUniqueId()));
     }
 
 }
